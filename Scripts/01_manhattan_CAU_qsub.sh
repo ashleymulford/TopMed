eb50e209aa26f18294033a8ae28d0a69bc733187
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-01-22 10:14:23 -0600

    Add files via upload

---
+++ Scripts/01_manhattan_CAU_qsub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N TopMed_Manhattan_CAU
+#PBS -S /bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l nodes=1:ppn=1
+#PBS -l mem=8gb
+#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
+#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
+cd $PBS_O_WORKDIR
+
+
+Rscript /home/ashley/TopMed/Scripts/01_man_CAU_run.R
+
+
+
+

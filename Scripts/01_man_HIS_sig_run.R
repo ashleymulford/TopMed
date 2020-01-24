3c8b058babb6e549337c7df8d23dd8d96b29145f
ashleymulford <35783933+ashleymulford@users.noreply.github.com>
2020-01-24 13:49:39 -0600

    Update 01_man_HIS_sig_run.R

--- Scripts/01_man_HIS_sig_run.R
+++ Scripts/01_man_HIS_sig_run.R
@@ -1,3 +1,4 @@
+#Import libraries
 library(data.table)
 library(dplyr)
 library(qqman)
@@ -10,9 +11,11 @@ library(tibble)
 #Create color vector
 colors<-sequential_hcl(4,"SunsetDark")
 
+#Read in file and filter for only significant p-vals (threshold chosen: 5e-20)
 HIS_pqtl<-fread("/home/ashley/TopMed/trans_eQTLs_HIS_pqtl_trans_10e-4.txt")
 HIS_pqtl_sig<-filter(HIS_pqtl, HIS_pqtl$pvalue<5e-20)
 
+#Generate list of proteins with significnat p-vals
 for (protein in HIS_pqtl_sig$gene){
   if(exists("proteins_list")){
     proteins_list<-append(proteins_list, protein)}
@@ -20,8 +23,10 @@ for (protein in HIS_pqtl_sig$gene){
     proteins_list<-c(protein)}
 }
 
+#Set for unique list
 protein_set<-unique(proteins_list)
 
+#Using set, subset original file to get all snps for proteins with sig hits
 for(protein in protein_set){
   if(exists("HIS_pqtl_man")){
     HIS_pqtl_man<-rbind(HIS_pqtl_man, filter(HIS_pqtl, HIS_pqtl$gene==protein))
@@ -31,7 +36,7 @@ for(protein in protein_set){
   }
 }
 
-
+#Split snp col into two cols, chr and bp
 for(snp in HIS_pqtl_man$snps){
   snpID<-strsplit(snp, ":")
   chrom<-snpID[[1]][1]
@@ -50,10 +55,11 @@ for(snp in HIS_pqtl_man$snps){
     bp_list<-c(bp)}
 }  
 
+#Add cols
 HIS_pqtl_man<-add_column(HIS_pqtl_man, chr = chr_list, .before = "gene")
 HIS_pqtl_man<-add_column(HIS_pqtl_man, bp = bp_list, .before = "gene")
 
-
+#Make manhattan plots for each sig protein
 for(protein in protein_set){
   HIS_pqtl_manh<-filter(HIS_pqtl_man, HIS_pqtl_man$gene==protein)
   png(filename = "/home/ashley/TopMed/Sig_Man_Plots/HIS_pqtl_" %&% protein %&% ".manplot.png", res=100)
@@ -63,6 +69,8 @@ for(protein in protein_set){
 
 
 
+#Identidfy possible trans proteins:
+#Create new dataframe with all snps with pval<5e-8
 for(protein in protein_set){
   HIS_pqtl_manh<-filter(HIS_pqtl_man, HIS_pqtl_man$gene==protein)
   for (pval in HIS_pqtl_manh$pvalue) {
@@ -77,13 +85,14 @@ for(protein in protein_set){
   }
 }
 
-
+#Create new dataframe with only chr and protein id, keep only unique rows (ie chr/proteins combos)
 HIS_peaks_chrs<-distinct(select(HIS_pqtl_manha, 2,4))
+#Keep only duplicate proteins (ie proteins will more than one chr with hit at pval<5e-8, possibly trans)
 HIS_peaks_chrs_trans<-HIS_peaks_chrs[HIS_peaks_chrs$gene %in% HIS_peaks_chrs$gene[duplicated(HIS_peaks_chrs$gene)],]
 fwrite(HIS_peaks_chrs_trans, "/home/ashley/TopMed/trans_proteins_HIS.txt", sep = "\t")
 
 
-
+#Get list of possible trans proteins
 for (protein in HIS_peaks_chrs_trans$gene){
   if(exists("trans_proteins_list")){
     trans_proteins_list<-append(trans_proteins_list, protein)}
@@ -91,12 +100,14 @@ for (protein in HIS_peaks_chrs_trans$gene){
     trans_proteins_list<-c(protein)}
 }
 
+#Make set
 trans_protein_set<-unique(trans_proteins_list)
 
-
+#Make manhattan plots for each possible trans protein
 for(protein in trans_protein_set){
   HIS_pqtl_manhatt<-filter(HIS_pqtl_man, HIS_pqtl_man$gene==protein)
   png(filename = "/home/ashley/TopMed/Sig_Trans_Man_Plots/HIS_pqtl_" %&% protein %&% ".manplot.png", res=100)
   manhattan(HIS_pqtl_manhatt, chr = "chr", bp = "bp", p = "pvalue", col = colors)
   dev.off()
 }
+
